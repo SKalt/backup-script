75ea6c3b98e9ff14373821ae2d31d72690746678
Unknown <kalt.steven@gmail.com>
2017-12-16 15:10:02 -0500

    Added initial usage script

---
+++ src/usage.sh
@@ -0,0 +1,14 @@
+program_name=$0
+function usage {
+cat <<help_message
+Usage: $program_name [[-s|--setup][-h|--help]]
+
+a backup script
+
+Options:
+  -s|--setup      generate a config file and exit.
+  -h|--help       Show this message and exit.
+help_message
+}
+usage
+exit
